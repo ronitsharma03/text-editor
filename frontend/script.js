776b9c9e2620fea5ae21c048dc2afed94c707cd9
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-03-31 00:49:21 +0530

    Ready livepreview

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,10 @@
-function run(){
-    
+function run() {
+    let htmlCode = document.getElementById("html-file").value;
+    let cssCode = document.getElementById("style-css").value;
+    let jsCode = document.getElementById("javascript-file").value;
+    let iframe = document.getElementById("code-output");
+
+    iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    iframeDocument.body.innerHTML = htmlCode + "<style>"+cssCode+"</style";
+    iframe.contentWindow.eval(jsCode);
 }
